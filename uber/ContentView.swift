981f9c0ecf4b8d320390c69a83f908326c85d641
Mike Keller <kontakt@mike-keller.de>
2022-12-03 15:21:12 +0100

    Initial Commit

---
+++ uber/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  uber
+//
+//  Created by Mike Keller on 03.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uber/uberApp.swift
@@ -0,0 +1,17 @@
+//
+//  uberApp.swift
+//  uber
+//
+//  Created by Mike Keller on 03.12.22.
+//
+
+import SwiftUI
+
+@main
+struct uberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
